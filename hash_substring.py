8317a5328c0d17db4fabd9581189aa8719c1895a
rinalds14 <39654754+rinalds14@users.noreply.github.com>
2023-04-16 19:23:44 +0000

    should be working

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,30 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    input_type = input().strip().upper()
+    if input_type == 'F':
+        with open('input.txt', 'r') as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+    if input_type == "I":
+        pattern = input().strip()
+        text = input().strip()
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    p = 10**9 + 7
+    x = 263
+    pattern_hash = sum(ord(pattern[i]) * pow(x, i, p) for i in range(len(pattern))) % p
+    n = len(text)
+    m = len(pattern)
+    hashes = [None] * (n - m + 1)
+    s = text[n - m:]
+    hashes[n - m] = sum(ord(s[i]) * pow(x, i, p) for i in range(m)) % p
+    factor = pow(x, m - 1, p)
+    for i in range(n - m - 1, -1, -1):
+        hashes[i] = (x * hashes[i + 1] + ord(text[i]) - factor * ord(text[i + m])) % p
+    return [i for i in range(n - m + 1) if hashes[i] == pattern_hash]
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
