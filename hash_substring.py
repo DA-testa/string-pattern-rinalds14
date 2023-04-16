6dd341164b1c010d6fb79c5f212fe49dcbfbd642
rinalds14 <39654754+rinalds14@users.noreply.github.com>
2023-04-16 19:59:47 +0000

    testing again?

--- hash_substring.py
+++ hash_substring.py
@@ -9,7 +9,6 @@ def read_input():
         pattern = input().strip()
         text = input().strip()
 
-    return pattern, text
 
 
 def print_occurrences(output):
@@ -17,35 +16,23 @@ def print_occurrences(output):
 
 
 def get_occurrences(pattern, text):
-    p = 1000000007
-    x = 1
+    pattern_len = len(pattern)
+    text_len = len(text)
+    d = 256
+    q = 1000000007
+    h = pow(d, pattern_len-1) % q
+
+    pattern_hash = sum(ord(pattern[i]) * pow(d, pattern_len - i - 1, q) % q for i in range(pattern_len)) % q
+    text_hash = sum(ord(text[i]) * pow(d, pattern_len - i - 1, q ) % q for i in range(pattern_len)) % q
     occurrences = []
 
-    pattern_hash = 0
-    for i in range(len(pattern)):
-        pattern_hash = (pattern_hash + ord(pattern[i]) * x) % p
-        x = (x * 263) % p
-
-    n = len(text)
-    m = len(pattern)
-
-    if n < m:
-        return []
-
-    hash_values = [0] * (n - m + 1)
-    hash_values[-1] = hash(text[n - m :])
-    x = 1
-    for i in range(m):
-        x = (x * 263) % p
-
-    for i in range(n - m - 1, -1, -1):
-        hash_values[i] = (263 * hash_values[i + 1] + ord(text[i]) - x * ord(text[i + m])) % p
-
-    for i in range(n - m + 1):
-        if pattern_hash == hash_values[i] and text[i : i + m] == pattern:
+    for i in range(text_len - pattern_len + 1):
+        if pattern_hash == text_hash and pattern == text[i : i + pattern_len]:
             occurrences.append(i)
-
+        if i < text_len - pattern_len:
+            text_hash = (d * (text_hash - ord(text[i]) * h) + ord(text[i+pattern_len])) % q
     return occurrences
+
 
 
 if __name__ == "__main__":
