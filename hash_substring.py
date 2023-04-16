72e445d459e17644056c592135b43bc535ff54de
rinalds14 <39654754+rinalds14@users.noreply.github.com>
2023-04-16 19:49:16 +0000

    testing?

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,52 @@
+#221RDB489 Rinalds Dobelis 16.grupa
 def read_input():
-    input_type = input().strip().upper()
-    if input_type == 'F':
-        with open('tests/06', 'r') as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
-    if input_type == "I":
+    choose = input().strip()
+    if choose == "F":
+        with open("tests/06") as file:
+            pattern = file.readline().strip()
+            text = file.readline().strip()
+    if choose == "I":
         pattern = input().strip()
         text = input().strip()
+
     return pattern, text
 
 
 def print_occurrences(output):
-    print(' '.join(map(str, output)))
+    print(" ".join(str(x) for x in output))
 
 
 def get_occurrences(pattern, text):
-    p = 10**9 + 7
-    x = 263
-    pattern_hash = sum(ord(pattern[i]) * pow(x, i, p) for i in range(len(pattern))) % p
+    p = 1000000007
+    x = 1
+    occurrences = []
+
+    pattern_hash = 0
+    for i in range(len(pattern)):
+        pattern_hash = (pattern_hash + ord(pattern[i]) * x) % p
+        x = (x * 263) % p
+
     n = len(text)
     m = len(pattern)
-    hashes = [None] * (n - m + 1)
-    hashes[n - m] = sum(ord(text[i]) * pow(x, i, p) for i in range(n - m, n)) % p
-    factor = pow(x, m - 1, p)
+
+    if n < m:
+        return []
+
+    hash_values = [0] * (n - m + 1)
+    hash_values[-1] = hash(text[n - m :])
+    x = 1
+    for i in range(m):
+        x = (x * 263) % p
+
     for i in range(n - m - 1, -1, -1):
-        hashes[i] = (x * hashes[i + 1] + ord(text[i]) - factor * ord(text[i + m])) % p
-    return [i for i in range(n - m + 1) if hashes[i] == pattern_hash]
+        hash_values[i] = (263 * hash_values[i + 1] + ord(text[i]) - x * ord(text[i + m])) % p
+
+    for i in range(n - m + 1):
+        if pattern_hash == hash_values[i] and text[i : i + m] == pattern:
+            occurrences.append(i)
+
+    return occurrences
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_occurrences(get_occurrences(*read_input()))
